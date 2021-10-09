20c11b50e6cb07ee2ef86beb70c42b9d821cd0b0
Japaneixxx <Japaneix@gmail.com>
2021-10-09 14:38:07 -0300

    acommit

--- python/hashtag/aula 1.py
+++ python/hashtag/aula 1.py
@@ -1,5 +1,28 @@
 import pyautogui
 import pyperclip
 
+pyautogui.PAUSE = 1
 #1 abrir o github
-#2 Clicar 
+
+pyautogui.press("win")
+pyautogui.write("Github Desktop")
+pyautogui.press("enter")
+
+#1.5 descrever comit
+
+pyautogui.press("f11")
+pyautogui.click(x=134, y=861)
+pyautogui.hotkey("ctrl, a")
+pyautogui.press("delete")
+pyautogui.write("commit")
+
+
+#2 Clicar em commit
+
+
+pyautogui.click(x=116, y=1017)
+
+#3 sair do github
+
+pyautogui.press("f11")
+pyautogui.hotkey("alt", "tab")
