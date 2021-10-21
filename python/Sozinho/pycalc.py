3487d103d587144868e7cfbb83866f3d84503637
Japaneixxx <Japaneix@gmail.com>
2021-10-21 00:11:28 -0300

    Create pycalc.py

---
+++ python/Sozinho/pycalc.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+# Filename: pycalc.py
+
+"""PyCalc is a simple calculator built using Python and PyQt5."""
+
+import sys
+
+# Import QApplication and the required widgets from PyQt5.QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLineEdit, QPushButton, QVBoxLayout
+
+__version__ = '0.1'
+__author__ = 'Leodanis Pozo Ramos'
+
+# Create a subclass of QMainWindow to setup the calculator's GUI
+
+
+class PyCalcUi(QMainWindow):
+    """PyCalc's View (GUI)."""
+
+    def _createDisplay(self):
+        self.display = QLineEdit()
+        self.display.setFixedHeight(35)
+        self.display.setAlignment(Qt.AlignRight)
+        self.display.setReadOnly(True)
+        self.generalLayout.addWidget(self.display)
+
+    def _createButtons(self):
+        self.buttons = {}
+        buttonsLayout = QGridLayout()
+
+        buttons = {'7': (0, 0),
+                   '8': (0, 1),
+                   '9': (0, 2),
+                   '/': (0, 3),
+                   'C': (0, 4),
+                   '4': (1, 0),
+                   '5': (1, 1),
+                   '6': (1, 2),
+                   '*': (1, 3),
+                   '(': (1, 4),
+                   '1': (2, 0),
+                   '2': (2, 1),
+                   '3': (2, 2),
+                   '-': (2, 3),
+                   ')': (2, 4),
+                   '0': (3, 0),
+                   '00': (3, 1),
+                   '.': (3, 2),
+                   '+': (3, 3),
+                   '=': (3, 4)
+                   }
+
+        for btnText, pos in buttons.items():
+            self.buttons[btnText] = QPushButton(btnText)
+            self.buttons[btnText].setFixedSize(40, 40)
+            buttonsLayout.addWidget(self.buttons[btnText], pos[0], pos[1])
+
+        self.generalLayout.addLayout(buttonsLayout)
+
+    def __init__(self):
+        """View initializer."""
+        super().__init__()
+        # Set some main window's properties
+        self.setWindowTitle('PyCalc')
+        self.setFixedSize(235, 235)
+        # Set the central widget
+        self.generalLayout = QVBoxLayout()
+        self._centralWidget = QWidget(self)
+        self.setCentralWidget(self._centralWidget)
+        self._centralWidget.setLayout(self.generalLayout)
+
+        self._createDisplay()
+        self._createButtons()
+
+# Client code
+
+
+def main():
+    """Main function."""
+    # Create an instance of QApplication
+    pycalc = QApplication(sys.argv)
+    # Show the calculator's GUI
+    view = PyCalcUi()
+    view.show()
+    # Execute the calculator's main loop
+    sys.exit(pycalc.exec_())
+
+
+if __name__ == '__main__':
+    main()
